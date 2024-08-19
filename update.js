91532a6a346bc643f5aee91c68094a2f015267be
Noah <kalervo.noah@gmail.com>
2024-08-20 04:10:24 +1000

    Update update.js

--- update.js
+++ update.js
@@ -10,11 +10,14 @@ const URL = `raw.githubusercontent.com/${OPTIONS.github}/${OPTIONS.repository}/$
 export async function main(ns) {
 	if (!await ns.prompt("Did you backup your files? (run 'download *' in terminal, and backup in Augmentations)")) { return }
 	let toDownload = await getFilesNeededToUpdate()
-	if (!await ns.prompt(`Do you allow update.js to overwrite the following files: (${toDownload.map(data => data[0]).join(", ")})`)) { return }
-	
+	if (!await ns.prompt(`Do you allow ${ns.getScriptName()} to overwrite the following files: (${toDownload.map(data => data[0]).join(", ")})`)) { return }
+
+	ns.tprint("Starting...")
 	for (let data of toDownload) {
-		ns.wget(data[1], data[0], "home")
+		ns.tprint(`Overwriting ${data[0]}`)
+		await ns.wget(data[1], data[0], "home")
 	}
+	ns.tprint("Successfully updated scripts.")
 }
 
 async function getFilesNeededToUpdate() {
